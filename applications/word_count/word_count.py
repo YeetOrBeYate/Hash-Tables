2e7c6ae7ecf2ea10c167f9988868e853e2ffd849
Kyle Corbin <kylecorbin26@gmail.com>
2020-05-07 18:14:24 -0400

    Got the word count file done. It isn't pretty but it works

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,9 +1,41 @@
+import re
+
+
 def word_count(s):
+    cache = {}
+    
     # Implement me.
+
+    #must use toLower
+    #
+
+    yeet = s.split()
+
+    foot = []
+
+    for x in yeet:
+        y = re.sub('[!@#$,".:;+=\/|{}()*^&-]', '', x)
+        y = y.replace("[]", "")
+        y = y.replace("\\", "")
+        y = y.lower()
+        foot.append(y)
+
+    for z in foot:
+        if z not in cache:
+            cache[z] = 1
+        else:
+            cache[z] +=1
+
+    empty = ''
+    if empty in cache:
+        del cache[empty]
+
+    return cache
 
 
 if __name__ == "__main__":
-    print(word_count(""))
+    print(word_count("[]"))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('":;,.-+=/\\|[]{}()*^&'))
