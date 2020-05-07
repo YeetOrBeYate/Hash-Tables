248ce625d965a06ce71115a82582e968eb2ed882
Kyle Corbin <kylecorbin26@gmail.com>
2020-05-07 16:10:49 -0400

    Got the expensive file done

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,5 +1,19 @@
+
+cache = {}
+
 def expensive_seq(x, y, z):
-    # Implement me
+
+    tup = (x, y, z)
+  
+    if x <=0:
+        return y + z
+    
+    if tup not in cache:
+        cache[tup] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+
+    return cache[tup]
+
+
 
 if __name__ == "__main__":
     for i in range(10):
--- hashtable/class.py
+++ hashtable/class.py
@@ -14,3 +14,44 @@
 # when you del()
 #     if load < .2:
 #         havle the capacioty/down to some minimum
+
+
+# import math
+
+# cache = {}
+
+# def inv_sqr_root(n):
+#     if n not in cache:
+#         cache[n] = 1/math.sqrt(n)
+
+#     return cache[n]
+
+# for i in range(1,6):
+#     print(inv_sqr_root(i))
+
+
+d = {
+    'foo':12,
+    'bar':17,
+    'qux': 3
+}
+
+# # sort by key
+
+# items = list(d.items())
+
+# print(items)
+
+# items.sort()
+
+# print(items)
+
+# sort by value
+
+items = list(d.items())
+
+print(items)
+
+items.sort(key=lambda e: e[1])
+
+print(items)
