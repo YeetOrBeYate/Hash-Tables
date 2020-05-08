efb6dde057f6a5b34f25f59484780f616a77dcd9
Kyle Corbin <kylecorbin26@gmail.com>
2020-05-08 03:10:10 -0400

    markov hard

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,9 +1,56 @@
 import random
+import sys
+sys.setrecursionlimit(10000)
 
 # Read in all the words in one go
 with open("input.txt") as f:
     words = f.read()
 
+    traverse = words.split()
+
 # TODO: analyze which words can follow other words
 
 # TODO: construct 5 random sentences
+
+class HashTableEntry:
+
+    def __init__(self,key, value):
+        self.key = key
+        self.value = value
+        #next is only for collisions
+        self.next = None
+
+    def __repr__(self):
+        return f"${self.value}--next:{self.next}"
+
+
+cache = {}
+
+
+def insert():
+    for x in range(0, len(traverse[0:400])):
+        for y in range(0, len(traverse)-1):
+            if traverse[y].lower() == traverse[x].lower():
+                if traverse[x] not in cache:
+                    cache[traverse[x]] = HashTableEntry(traverse[x], traverse[y+1])
+                else:
+                    node = cache[traverse[x]]
+                    
+                    if node.key == traverse[x] and node.value == traverse[y+1]:
+                        node.value = traverse[y+1]
+                    else:
+                        node = cache[traverse[x]]
+                        while node.next is not None:
+                            node = node.next
+
+                            if node.key == traverse[x] and node.value == traverse[y+1]:
+                                node.value = traverse[y+1]
+                                        
+
+                        node.next = HashTableEntry(traverse[x],traverse[y+1])
+
+    print(cache)
+
+            
+
+insert()
--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,10 +1,29 @@
 def no_dups(s):
     # Implement me.
 
+    cache = {}
+
+    string = s.split()
+
+
+    returnString = ""
+
+    for x in string:
+        if x not in cache:
+            cache[x] = x
+            if x != len(string):
+                returnString += cache[x] + " "
+            else:
+                returnString += cache[x]
+            
+
+            
+    
+    return returnString.rstrip()
 
 if __name__ == "__main__":
     print(no_dups(""))
     print(no_dups("hello"))
-    print(no_dups("hello hello"))
+    print(no_dups("hello hello poop"))
     print(no_dups("cats dogs fish cats dogs"))
     print(no_dups("spam spam spam eggs spam sausage spam spam and spam"))
